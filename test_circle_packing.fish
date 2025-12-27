8f50afc1693dbbc7099af72f8cdaaf609c69da23
ifsheldon <feng.liang@kaust.edu.sa>
2025-12-27 13:13:42 +0800

    update test script

--- test_circle_packing.fish
+++ test_circle_packing.fish
@@ -1,3 +1,10 @@
-set -x GEMINI_API_KEY "TODO"
-set -x OPENAI_API_KEY "TODO"
+# Load GEMINI_API_KEY and OPENAI_API_KEY from ../.env and export them
+for key in GEMINI_API_KEY OPENAI_API_KEY
+    set -l value (grep "^$key=" ../.env | cut -d'=' -f2-)
+    if test -n "$value"
+        set -x $key $value
+    else
+        echo "Warning: $key not found in ../.env" >&2
+    end
+end
 uv run shinka_launch variant=circle_packing_example_test
